580137c260ee3838955f9f2de6cab2e0aaebe696
Gabriel (Tavares) Dos Santos <162151479+GTavaresDev@users.noreply.github.com>
2024-06-03 21:30:01 -0300

    Update main.c

--- Projeto IP/main.c
+++ Projeto IP/main.c
@@ -3,9 +3,6 @@
 #include <time.h> 
 #include "funcoes.h"
 
-// Preciso encontrar uma forma de voltar para o menu sem que o programa seja fechado antes 
-// Se eu usar um do While e colca esse switch dentro funciona ?? 
-// Função principal
 int main() {
     menu();
 }
